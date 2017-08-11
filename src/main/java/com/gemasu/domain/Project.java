48a5e8c3f5882286780a43d09a756222389fa3fa
Germán Segura <german.andres.segura@gmail.com>
2017-08-11 11:32:57 -0500

    mysql config

--- src/main/java/com/gemasu/domain/Project.java
+++ src/main/java/com/gemasu/domain/Project.java
@@ -2,7 +2,12 @@ package com.gemasu.domain;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Project {
+	@Id
 	private String id;
 	private String name;
 	private Date StartDate;
