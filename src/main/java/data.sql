80037a69f8c436dda9d2b64a8dd6155c27b67a4c
alien <german.andres.segura@gmail.com>
2017-08-12 16:49:08 -0500

    Error fixed

--- src/main/java/data.sql
+++ src/main/java/data.sql
@@ -1,6 +1,5 @@
 
 INSERT INTO `organization` (`id`, `name`, `status`) VALUES
-<<<<<<< HEAD
 	(1, 'MUM', 1),
 	(2, 'WALMART', 1),
 	(3, 'LOGLIS', 0);
@@ -9,8 +8,3 @@ INSERT INTO `organization` (`id`, `name`, `status`) VALUES
 
 INSERT INTO `project` (`id`, `start_date`, `category`, `end_date`, `max_num_of_memebers`, `name`, `status`, `organization_id`) VALUES
 (1, '2017-08-08', 'TUTOR', '2017-08-23', 10, 'ENGLISH CLASSES', 'NEW', 1);
-=======
-	(1, 'MUM', "ACTIVE"),
-	(2, 'WALMART', "ACTIVE"),
-	(3, 'LOGLIS', "INACTIVE");
->>>>>>> branch 'master' of https://github.com/maggiedemeke/MUMVolunteerWSProject.git
