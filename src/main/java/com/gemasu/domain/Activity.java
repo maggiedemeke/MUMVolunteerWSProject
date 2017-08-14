0507673a7badc4d6defbd0ec76f00e06e512820f
maggiedemeke <maggie>
2017-08-14 11:55:25 -0500

    Modifications on Activity and Project services

--- src/main/java/com/gemasu/domain/Activity.java
+++ src/main/java/com/gemasu/domain/Activity.java
@@ -7,16 +7,30 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 @Entity
 public class Activity {
-	
-	@Id @GeneratedValue
+
+	@Id
 	public int id;
+	
 	@ManyToOne
-	@JoinColumn(name="projectId")
+	@JoinColumn(name = "projectId")
 	public Project project;
 	
 	private String name;
+	
+	
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
 	
 
 	public String getName() {
@@ -33,6 +47,15 @@ public class Activity {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", project=" + project + ", name=" + name + "]";
+	}
+
+	public Activity() {
+		
 	}
 	
 
--- src/main/java/data.sql
+++ src/main/java/data.sql
@@ -1,10 +1,16 @@
 
 INSERT INTO `organization` (`id`, `name`, `status`) VALUES
-	(1, 'MUM', 1),
-	(2, 'WALMART', 1),
-	(3, 'LOGLIS', 0);
+	(1, 'MUM', "ACTIVE"),
+	(2, 'WALMART', "ACTIVE"),
+	(3, 'LOGLIS', "ACTIVE");
 
 
 
 INSERT INTO `project` (`id`, `start_date`, `category`, `end_date`, `max_num_of_memebers`, `name`, `status`, `organization_id`) VALUES
 (1, '2017-08-08', 'TUTOR', '2017-08-23', 10, 'ENGLISH CLASSES', 'NEW', 1);
+
+
+INSERT INTO `activity` (`id`, `name`, `project_id`) VALUES
+	(1, 'tutorial1', 1),
+	(2, 'tutorial2', 1),
+	(3, 'tutorial3', 1);
