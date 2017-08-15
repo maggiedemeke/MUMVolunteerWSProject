f4fcebfc50c9fcd9abbe944607a36a15f22a8f60
alien <german.andres.segura@gmail.com>
2017-08-15 14:47:02 -0500

    ActivityController fix Annotation

--- src/main/java/com/gemasu/controller/ActivityController.java
+++ src/main/java/com/gemasu/controller/ActivityController.java
@@ -4,17 +4,18 @@ package com.gemasu.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.gemasu.domain.Activity;
 import com.gemasu.service.ActivityService;
 import com.gemasu.service.ProjectService;
 
-@Controller 
+@RestController
 @RequestMapping("/activity")
 public class ActivityController {
 	
@@ -44,6 +45,7 @@ public class ActivityController {
 
 	@RequestMapping("/projectactivity/{id}")
 	public List<Activity> getAllActivities(@PathVariable int id) {
+		System.out.println("Activities by Project");
 		List<Activity> activities = activityService.getAllActivities(projectService.getProject(id));
 		return activities;
 	}
