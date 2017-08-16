b785b8401545c789939e748a78854a4a196ee123
alien <german.andres.segura@gmail.com>
2017-08-16 11:39:18 -0500

    Role is data fixed

--- src/main/java/data.sql
+++ src/main/java/data.sql
@@ -27,6 +27,6 @@ INSERT INTO `user` (`id`, `email`, `first_name`, `last_name`, `hours`, `password
 
 
 INSERT INTO `role` (`id`, `role`, `user_id`) VALUES
-	(1, 'MEMBER', 1),
-	(2, 'ADMIN', 1);
+	(1, 'ROLE_MEMBER', 1),
+	(2, 'ROLE_ADMIN', 1);
 	
